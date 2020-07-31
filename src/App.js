98989e3ffa357176270634c6f9f24e3c1f172ebe
Fahad Shaikh <linkfahad@hotmail.com>
2020-08-01 00:12:08 +0500

    added services section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from "./components/NavBar";
 import TopAnimation from "./components/TopAnimation";
-
+import WhatWeDo from "./components/WhatweDo"
 function App() {
   
   return (
     <div>
       <NavBar />
       <TopAnimation />
+      <WhatWeDo />
     </div>
   );
 }
---
+++ src/components/WhatweDo.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Lottie from "react-lottie";
+import ServicesAnim from "./servicesAnim.json";
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import useWebAnimations, { slideInRight, } from "@wellyshen/use-web-animations";
+
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+    padding: 100,
+  },
+});
+
+const WhatWeDo = () => {
+  // const { ref } = useWebAnimations({ ...slideInRight})
+  const { keyframes, timing } = slideInRight;
+  const { ref } = useWebAnimations({
+  keyframes,
+  timing: {
+    ...timing,
+    // delay: 1000, // Delay 1s
+    iterations: 1,
+    duration: 10000, // Speed up the animation
+  },
+});
+  
+
+
+
+
+  const classes = useStyles();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: ServicesAnim,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  return (
+ 
+ 
+ 
+ 
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+    }}>
+
+      <div>
+        <Lottie
+          options={defaultOptions}
+          height={"100%"}
+          width={"100%"}
+        />
+      </div>
+
+      <div className="target" ref={ref}>
+        <div className={classes.root}>
+
+          <Typography variant="h3" gutterBottom>
+            Our Services
+        </Typography>
+          <Typography variant="h4" gutterBottom>
+            We Value Our Clients!
+        </Typography>
+
+          <Typography variant="caption" display="block" gutterBottom>
+            Give us a chance to serve you!
+        </Typography>
+          <Typography variant="overline" display="block" gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhatWeDo
+
